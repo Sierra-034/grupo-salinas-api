ed6621dd8daa310633e7286f369396c4923bf31b
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-11-27 08:34:19 -0600

    Basic config

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from app.config import get_settings, Settings
 
 app = FastAPI()
 
 
 @app.get('/')
-def index():
-    return {'Hello': 'world!'}
+def index(settings: Settings = Depends(get_settings)):
+    return {
+        'Hello': 'world!',
+        'environment': settings.environment,
+        'testing': settings.testing,
+    }
