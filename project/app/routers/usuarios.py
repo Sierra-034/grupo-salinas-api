b1c5a2bc55297f5635d80f1bd1ec2507183f6cdd
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-11-29 11:15:21 -0600

    Update User endpoint

--- project/app/routers/usuarios.py
+++ project/app/routers/usuarios.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, \
+    status
 from pytz import timezone
 
 from ..models import Usuario
 from ..schemas import UsuarioInSchema, UsuarioOutSchema, \
-    RoleType
+    UsuarioModRoleSchema
 from ..dependencies import is_this_very_first_user
+from ..dependencies import RoleChecker
 
 router = APIRouter(prefix='/users')
 
@@ -20,7 +22,7 @@ def create_user(usuario_in: UsuarioInSchema, is_very_first: ... = Depends(is_thi
     usuario: Usuario
     if is_very_first:
         usuario = Usuario(
-            **usuario_in.dict(), rol=RoleType.ADMINISTRADOR, create_at=creation_date)
+            **usuario_in.dict(), rol='administrador', create_at=creation_date)
     else:
         usuario = Usuario(**usuario_in.dict(),
                           created_at=datetime.now(tz=mexico_city))
@@ -28,3 +30,17 @@ def create_user(usuario_in: UsuarioInSchema, is_very_first: ... = Depends(is_thi
     usuario.password = hashed_password
     usuario.save()
     return usuario
+
+
+@router.put(
+    '/{user_name}', response_model=UsuarioOutSchema,
+    dependencies=[Depends(RoleChecker(['administrador']))],)
+def update_user(user_name: str, usuario_mod: UsuarioModRoleSchema):
+    query = Usuario.update(**usuario_mod.dict()
+                           ).where(Usuario.id == user_name).returning(Usuario)
+    user_updated = query.execute()
+    if len(user_updated) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Empleado con [id: {user_name}] no fué encontrado')
+
+    return user_updated[0].__data__
